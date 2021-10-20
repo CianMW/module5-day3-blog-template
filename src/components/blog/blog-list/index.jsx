2fb0c53c90dd6d8752608c20a6994972ba3770e7
CianMW <markwick.cian@gmail.com>
2021-10-20 16:51:07 +0100

    Setup

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -3,6 +3,17 @@ import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
 export default class BlogList extends Component {
+
+
+  fetchData = async () =>{
+
+    fetch('http://localhost:3001/posts')
+    .then(response => response.json())
+    .then(data => console.log("HERE ARE THE POSTS", data));
+  }
+  componentDidMount = () =>{
+    this.fetchData()
+  }
   render() {
     return (
       <Row>
