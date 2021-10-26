7a5b22a57eca008697938ef1a72a2a7bfb27fc34
CianMW <markwick.cian@gmail.com>
2021-10-26 22:21:41 +0100

    u

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -7,6 +7,7 @@ export default class Home extends Component {
   render() {
     return (
       <Container fluid="sm">
+        {console.log(process.env.REACT_APP_BE_REQS)}
         <h1 className="blog-main-title">Welcome to the Strive Blog!</h1>
         <BlogList />
       </Container>
